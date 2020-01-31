c964db6cbb716456ba08b76a25cf5abc1dfb65b1
ChristianStaley <36854153+ChristianStaley@users.noreply.github.com>
2020-01-31 12:47:45 +0000

    Time Scripts created

---
+++ FortSnow/Assets/Christian/CS_Scripts/Change_Time.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Change_Time : MonoBehaviour
+{
+    public DateTime fl_currentTime;
+    private Light l_worldLight;
+    string _time;
+    System.DateTime theTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        l_worldLight = GameObject.Find("Directional Light").GetComponent<Light>();
+        theTime = System.DateTime.Now;
+        _time = theTime.Hour + ":" + theTime.Minute + ":" + theTime.Second;
+
+        print(_time);
+
+        if (theTime.Hour <= 12)
+        {
+            l_worldLight.transform.Rotate(100, 100, 100);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
