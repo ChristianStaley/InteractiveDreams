f925860acd39e9310aad51326f59b61df90d7477
Manuelbonnet90 <58072925+Manuelbonnet90@users.noreply.github.com>
2020-01-31 11:32:14 +0000

    Scene

---
+++ FortSnow/Assets/Manuel/MB_Scripts/Snowballs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float fl_cooldown = 1F;
+    public float fl_accuracy = 100;
+    public int in_ammo = 1000;
+
+    // GameObjects 
+    public GameObject GO_snowball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Attack()
+    {
+        if (Input.GetButton("Fire1") && Time.time > fl_next_attack_time && in_ammo > 0)
+        {   // Reset the cooldown delay
+            fl_next_attack_time = Time.time + fl_cooldown;
+            // Reduce Ammo;
+            in_ammo--;
+            // Create Projectile slightly to the right and in front of the PC using the camera Rotation
+            GameObject _GO_snoowball_clone = Instantiate(GO_snowball, transform.position + transform.TransformDirection(new Vector3(0.25f, 0f, 1.5F)), transform.rotation);
+           
+        }
+    }//-----
+}
