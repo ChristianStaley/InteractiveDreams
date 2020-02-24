2a9b2570c4c0c2c44611f73bcb940d45fb2b48fc
ChristianStaley <36854153+ChristianStaley@users.noreply.github.com>
2020-02-24 11:31:41 +0000

    New Scripts and Terrain Changes
    New script to teleport the player back into the level.
    New script to disable renderer for triggers.
    Fixed snow terrain clipping with building.

---
+++ FortSnow/Assets/Christian/CS_Scripts/TriggerTeleport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTeleport : MonoBehaviour
+{
+    public GameObject teleportExit;
+    public GameObject target;
+
+    private Transform teleportLocation;
+    private CharacterController targetController;
+
+    private void Start()
+    {
+        if(teleportExit != null)
+            teleportLocation = teleportExit.GetComponent<Transform>();
+
+        if(target != null)
+        targetController = target.GetComponent<CharacterController>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == target.name)
+        {
+            targetController.enabled = false;
+            target.transform.position = teleportLocation.position;
+            targetController.enabled = true;
+        }
+    }
+
+}
---
+++ FortSnow/Assets/Christian/CS_Scripts/TriggerVisibility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerVisibility : MonoBehaviour
+{
+    public bool isVisible = false;
+
+    void Update()
+    {
+        if (isVisible)
+        {
+            gameObject.GetComponent<MeshRenderer>().enabled = true;
+        }
+        else
+            gameObject.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
