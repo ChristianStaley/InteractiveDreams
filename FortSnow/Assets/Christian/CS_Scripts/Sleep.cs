ee0a353a8e6ec8e1dd67fe89457c768b230e38df
ChristianStaley <36854153+ChristianStaley@users.noreply.github.com>
2020-02-28 12:48:54 +0000

    Menu Buttons

---
+++ FortSnow/Assets/Christian/CS_Scripts/Sleep.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Sleep : MonoBehaviour
+{
+    public GameObject panelExit;
+    private GameObject go_PC;
+
+    private bool confirm = false;
+
+    private void Start()
+    {
+        go_PC = GameObject.Find("FPSController");
+        panelExit.SetActive(false);
+    }
+
+    private void Update()
+    {
+
+        if (Vector3.Distance(go_PC.transform.position, gameObject.transform.position) > 3)
+        {
+            panelExit.SetActive(false);
+            confirm = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && Input.GetKeyDown("e"))
+        {
+            panelExit.SetActive(true);
+            confirm = true;
+
+        }
+
+        if(other.tag == "Player" && confirm && Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene("StartScreen");
+        }
+
+    }
+}
