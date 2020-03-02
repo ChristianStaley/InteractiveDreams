9a9fcc653fe0fca13f6dd67bb5bf19fe5576c32b
ChristianStaley <36854153+ChristianStaley@users.noreply.github.com>
2020-03-02 10:33:43 +0000

    Interaction scripts updated, Menu screens updated
    The player can now pickup objects such as collectables.
    the main menu now allows for navigation to the level, credits and exit the game.

---
+++ FortSnow/Assets/Christian/CS_Scripts/PC_Interact.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Interact : MonoBehaviour
+{
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("FirstPersonCharacter").GetComponent<Camera>();
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        if (Input.GetKeyDown("e"))
+        {
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                print("I'm looking at " + hit.transform.name);
+                if (hit.transform.gameObject.CompareTag("Pickupable"))
+                {
+                    print("Message sent");
+                    hit.transform.gameObject.GetComponent<PickupObject>().PlayerInteraction(cam.transform);
+
+                    //SendMessageUpwards("PlayerInteraction", cam.transform, SendMessageOptions.DontRequireReceiver);
+                    
+                    //SendMessage("PlayerInteraction", cam.transform, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+
+            else
+                print("I'm looking at nothing!");
+        }
+    }
+}
--- FortSnow/Assets/Christian/CS_Scripts/PickupObject.cs
+++ FortSnow/Assets/Christian/CS_Scripts/PickupObject.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class PickupObject : MonoBehaviour
 {
+    bool interacted = false;
+    Transform moveLocation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("e"))
+        if (interacted)
         {
-
+            transform.position = Vector3.MoveTowards(transform.position, moveLocation.transform.position, 5f * Time.deltaTime);
         }
+    }
+
+    public void PlayerInteraction(Transform playerLocation)
+    {
+        print("Recieved");
+        interacted = true;
+        moveLocation = playerLocation;
+        gameObject.GetComponent<Collider>().enabled = false;
+        Destroy(gameObject, 0.5f);
     }
 
 
