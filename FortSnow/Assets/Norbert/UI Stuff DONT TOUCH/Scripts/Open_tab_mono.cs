4572ad501c06a6fa8b05888e8e7f00996a99d47f
ChristianStaley <36854153+ChristianStaley@users.noreply.github.com>
2020-03-05 20:42:23 +0000

    Implemented Start UI graphics

---
+++ FortSnow/Assets/Norbert/UI Stuff DONT TOUCH/Scripts/Open_tab_mono.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Open_tab_mono : MonoBehaviour
+{
+    GameObject Pause_menu_open;
+    GameObject Escape_button_open;
+
+    public bool pause_menu = false;
+    public bool escape_button_vanish = false;
+    public bool escape_button_spawn = false;
+
+    private void Start()
+    {
+        Pause_menu_open = GameObject.Find("Pause_menu");
+        Escape_button_open = GameObject.Find("Escape_button");
+
+        Close_All();
+    }
+
+    public void Close_All()
+    {
+        Pause_menu_open.transform.localScale = new Vector3(0.001F, 0.001F, 0.001F);
+        //Escape_button_open.transform.localScale = new Vector3(0.001F, 0.001F, 0.001F);
+    }
+
+    public void Open_tab()
+    {
+        //Close_All();
+
+        if (pause_menu == true)
+        {
+            Pause_menu_open.transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (escape_button_vanish == true)
+        {
+            Escape_button_open.transform.localScale = new Vector3(0.001f, 0.001f, 0.001f);
+        }
+
+        if (escape_button_spawn == true)
+        {
+            Escape_button_open.transform.localScale = new Vector3(5, 5, 5);
+        }
+    }
+}
